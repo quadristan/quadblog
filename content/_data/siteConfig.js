ac05c2cd1cf7562bc5c3f4d7883d1805900cec2b
quadristan <quadristan@protonmail.com>
2024-07-16 20:01:51 +0200

    fix urls

--- content/_data/siteConfig.js
+++ content/_data/siteConfig.js
@@ -18,7 +18,7 @@ module.exports = {
     // Production URL. Used by 'alwaysProductionUrl' filter.
     // If none is provided, relative paths will be used
     // and in some cases (like sitemap.xml) that is TOTALLY NOT what you want
-    url: process.env.URL || 'https://quadristan.fr',
+    url: process.env.URL || 'https://blog.quadristan.fr',
     // Image used as a site logo. If none is provided, site title as text
     // will be displayed as fallback.
     // If you opt for text fallback, you may want to adjust
@@ -71,7 +71,7 @@ module.exports = {
     name: 'Tristan Parisot',
     // Your website URL. Used in the footer. Optional.
     // Doesn't have to be the same link as this site
-    url: 'https://quadristan.fr',
+    url: 'https://blog.quadristan.fr',
     // Your fediverse links (e.g. Mastodon). You can provide more than one.
     // For the first account in this array your site will generate
     // .well-known/webfinger file for you so that you'll be searchable
